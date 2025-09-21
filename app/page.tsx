267b1573c093a5bec7da86bc57f226532b9144c4
noorajannat_naj <noor.jannat@vivasoftltd.com>
2025-09-22 00:37:59 +0600

    Shadcn button

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,25 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button"
+import Link from "next/link";
+
+export function LinkAsButton(props: any) {
+  console.log(props)
+  const {children, className} = props
+  return (
+   <div className={className}>
+     <Button asChild>
+      <Link href="/login">{children}</Link>
+    </Button>
+   </div>
+  )
+}
 
 export default function Home() {
   let hello;
+
   return (
-    <div>Hi</div>
+
+    <div>
+    <LinkAsButton className = "flex flex-wrap items-center justify-center min-h-screen">Login here</LinkAsButton>
+    </div>
   );
 }
